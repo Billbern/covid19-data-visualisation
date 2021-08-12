becc1e0968db383d8ad53bae93d5953979952afb
Billbern <denis.abaidoo@yahoo.com>
2021-08-12 11:10:14 +0000

    resolved link for mapplots

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 export default function SideBar() {
 
-    const [data] = useState([{title: 'Pie Charts', link: '/'}, {title: 'Bar Charts', link: '/barcharts'}, {title: 'Line Graphs', link: '/linegraphs'}, {title: 'Stacked Bar Charts', link: '/stackedbarcharts'}, {title: 'Scatter Plots', link: '/scatterplots'}, {title: 'Map Plot', link: '/mapplot'}])
+    const [data] = useState([{title: 'Pie Charts', link: '/'}, {title: 'Bar Charts', link: '/barcharts'}, {title: 'Line Graphs', link: '/linegraphs'}, {title: 'Stacked Bar Charts', link: '/stackedbarcharts'}, {title: 'Scatter Plots', link: '/scatterplots'}, {title: 'Map Plot', link: '/mapplots'}])
 
     return (
         <div className="col-span-2 bg-gray-300">
